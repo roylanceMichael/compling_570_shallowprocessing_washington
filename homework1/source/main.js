2c485dc692c1a003a2b3afbfb0e8751f4db054ca
Mike Roylance <roylance.michael@gmail.com>
2013-10-01 17:55:02 -0600

    updated a few things

--- homework1/source/main.js
+++ homework1/source/main.js
@@ -1,6 +1,22 @@
+var Tokenizer = require('./tokenizer.js');
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
-process.stdin.on("data", function(chunk){
-	console.log("data: " + chunk);
+var tmpStr = "";
+
+process.stdin.on("data", function(chunk) {
+	var splitLines = (tmpStr + chunk).split("\n");
+
+	for(var i = 0; i < splitLines.length - 1; i++) {
+		console.log("data: " + splitLines[i]);
+	}
+	tmpStr = splitLines[splitLines.length-1];
+});
+
+process.stdin.on("end", function(){
+	var splitLines = tmpStr.split("\n");
+
+	for(var i = 0; i < splitLines.length; i++) {
+		console.log("enddata: " + splitLines[i]);
+	}
 });
