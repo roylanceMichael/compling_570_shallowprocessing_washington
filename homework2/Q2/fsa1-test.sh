fe5aca96bf6653366cdacc571e561e8b4114ffe4
Mike Roylance <roylance.michael@gmail.com>
2013-10-06 13:22:08 -0600

    optimized a bit

--- homework2/Q2/fsa1-test.sh
+++ homework2/Q2/fsa1-test.sh
@@ -1,7 +1,9 @@
+echo '""' | carmel -sli fsa1
 echo '"a" "b"' | carmel -sli fsa1
 echo '"a"' | carmel -sli fsa1
 echo '"b"' | carmel -sli fsa1
 echo '"a" "a" "a" "b"' | carmel -sli fsa1
 echo '"a" "a" "a"' | carmel -sli fsa1
 echo '"a" "b" "b" "b"' | carmel -sli fsa1
-echo '"a" "b" "a"' | carmel -sli fsa1
+echo 'this should fail:'
+echo '"a" "b" "a"' | carmel -sli fsa1 #should be invalid
