b4627aed555ab2bd08319ed6f7864698ba718276
Mike Roylance <roylance.michael@gmail.com>
2013-10-06 14:26:42 -0600

    updated testing files

--- homework2/Q2/fsa3-test.sh
+++ homework2/Q2/fsa3-test.sh
@@ -1,9 +1,9 @@
-echo '""' | carmel -sli fsa3
 echo '"a" "b"' | carmel -sli fsa3
 echo '"b"' | carmel -sli fsa3 
 echo '"a" "a" "a" "b"' | carmel -sli fsa3
 echo '"a" "b" "b" "b"' | carmel -sli fsa3
 echo 'this should fail:'
+echo '""' | carmel -sli fsa3
 echo '"a" "a" "a"' | carmel -sli fsa3 # should be invalid
 echo '"a"' | carmel -sli fsa3 # should be invalid
 echo '"a" "b" "a"' | carmel -sli fsa3 #should be invalid
--- homework2/Q2/fsa4-test.sh
+++ homework2/Q2/fsa4-test.sh
@@ -1,8 +1,8 @@
-echo '""' | carmel -sli fsa4
 echo '"a" "b"' | carmel -sli fsa4
 echo '"a" "a" "a" "b"' | carmel -sli fsa4 
 echo '"a" "b" "b" "b"' | carmel -sli fsa4
 echo 'these should fail:'
+echo '""' | carmel -sli fsa4
 echo '"a" "a" "a"' | carmel -sli fsa4 # should be invalid
 echo '"a"' | carmel -sli fsa4 # should be invalid
 echo '"b"' | carmel -sli fsa4 # should be invalid
